5503cf56b97846143a9bdb1dceec8ac484fe461e
Telegram Bots <93692515+weRepo@users.noreply.github.com>
2021-11-17 11:04:50 +0530

    Update __aimultilanguage.py

--- innexiaBot/modules/__aimultilanguage.py
+++ innexiaBot/modules/__aimultilanguage.py
@@ -3,12 +3,12 @@ import re
 import os
 import html
 import requests
-import TGNRobot.modules.sql.kuki_sql as sql
+import innexiaBot.modules.sql.kuki_sql as sql
 
 from time import sleep
 from telegram import ParseMode
-from TGNRobot import dispatcher, updater, SUPPORT_CHAT
-from TGNRobot.modules.log_channel import gloggable
+from innexiaBot import dispatcher, updater, SUPPORT_CHAT
+from innexiaBot.modules.log_channel import gloggable
 from telegram import (CallbackQuery, Chat, MessageEntity, InlineKeyboardButton,
                       InlineKeyboardMarkup, Message, ParseMode, Update, Bot, User)
 
@@ -18,8 +18,8 @@ from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
 
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 
-from TGNRobot.modules.helper_funcs.filters import CustomFilters
-from TGNRobot.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from innexiaBot.modules.helper_funcs.filters import CustomFilters
+from innexiaBot.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
 
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
@@ -68,7 +68,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Sophia Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
+                "BG's Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
